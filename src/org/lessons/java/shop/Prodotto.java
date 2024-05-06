6b8733508dd3b3e667f09b6d010d79ba9eaa37b6
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-05-06 19:27:05 +0200

    Impostazione generale dell'esercizio con commenti e primi metodi

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,85 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+//	Un prodotto è caratterizzato da: - codice (numero intero) - nome - descrizione - prezzo - iva
+	private long codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private double iva;
+	
+	public Prodotto (int codice, String nome, String descrizione, double prezzo, double iva) {
+		this.codice = generaCodice();
+		this.setNome(nome);
+		this.setDescrizione(descrizione);
+		this.setPrezzo(prezzo);
+		this.setIva(iva);
+	}
+
+	private long generaCodice() {
+		Random rand = new Random();
+		return rand.nextInt(10000000);
+	}
+	
+//	GETTER CODICE
+	public long getCodice() {
+		return codice;
+	}
+	
+
+//	GETTER E SETTER DI NOME
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+//	GETTER E SETTER DI descrizione
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+//	GETTER E SETTER DI PREZZO
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+//	GETTER E SETTER DI IVA
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	
+//	- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random 
+//		con un metodo creaNuovoProdotto da usare all'interno del main mi verrà restituito un prodotto con codice random:
+	
+//	Il prodotto potrebbe essere creato chiedendo all'utente il nome di un prodotto che si vuole creare.
+	
+	
+//	- il codice prodotto sia accessibile solo in lettura
+//		per fare questo dovrò usare il metodo getter
+//	- gli altri attributi siano accessibili sia in lettura che in scrittura
+//		in questo caso dovrò invece usare un metodo setter
+//	- il prodotto esponga un metodo per avere il prezzo base
+		
+//	- il prodotto esponga un metodo per avere il prezzo comprensivo di iva 
+//	- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome 
+//	Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto.
 }
+
